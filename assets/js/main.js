49a279e56640e2222de393509c0f70e0b2f7c810
Han Hua <ahua@groupon.com>
2014-04-14 23:39:02 -0500

    get instagram data back

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,25 @@
-var emotionCounts = {};
+var client_id = '73d111677079497b9c66f07ea1a4f1b0';
 
 $(function(){
-  var client_id = '73d111677079497b9c66f07ea1a4f1b0';
-
   var colorEmotions = {};
   _.each($('.background'), function(colorDom){
     colorEmotions[$(colorDom).attr('id')] = $(colorDom).css("background-color");
   });
 
-  // var emotionCounts = {};
+  var tagFetchTasks = {}
   _.each(colorEmotions, function(color, emotion){
-    $.ajax({
-      url: 'https://api.instagram.com/v1/tags/' + emotion + '?client_id=' + client_id,
-      dataType: "jsonp",
-      success: function(response){
-        emotionCounts[emotion] = response.data.media_count;
-      }
-    });
+    tagFetchTasks[emotion] = function(callback){
+      $.ajax({
+        url: 'https://api.instagram.com/v1/tags/' + emotion + '?client_id=' + client_id,
+        dataType: "jsonp",
+        success: function(response){
+          callback(null, response.data.media_count);
+        }
+      });
+    };
   });
 
-  console.log(JSON.stringify(emotionCounts, null, 3));
+  async.parallel(tagFetchTasks, function(err, results){
+    console.log(JSON.stringify(results, null, 3));
+  });
 });
-
